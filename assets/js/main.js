d4cb91a292107bd9284a5d546070b767384c76ac
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2022-05-17 18:27:32 +0400

    comments and product carousel added to single product page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,37 +1,55 @@
-$(document).ready(function(){
-    $('.story-carousel').owlCarousel({
-        loop:false,
-        margin:10,
-        dots:false,
-        responsive:{
-            0:{
-                items:4
-            },
-            600:{
-                items:7
-            },
-            1000:{
-                items:10
-            }
-        }
-    })
+$(document).ready(function () {
+  $(".story-carousel").owlCarousel({
+    loop: false,
+    margin: 10,
+    dots: false,
+    responsive: {
+      0: {
+        items: 4,
+      },
+      600: {
+        items: 7,
+      },
+      1000: {
+        items: 10,
+      },
+    },
+  });
 
-    $('.widgets-carousel').owlCarousel({
-        loop:false,
-        autoWidth:true,
-        dots:false,
-        mouseDrag:true,
-        pullDrag:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:6
-            }
-        }
-    })
-})
+  $(".widgets-carousel").owlCarousel({
+    loop: false,
+    autoWidth: true,
+    dots: false,
+    mouseDrag: true,
+    pullDrag: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 6,
+      },
+    },
+  });
+
+  $(".same-product-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots:false,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 3,
+      },
+      1000: {
+        items: 5,
+      },
+    },
+  });
+});
